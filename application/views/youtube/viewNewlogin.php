35ec2cf149de7a59e3a613b82dff712e0fbb752b
Pratik Anand <pratik.preet@gmail.com>
2015-12-19 21:24:37 +0530

    added icons to login page

--- application/views/youtube/viewNewlogin.php
+++ application/views/youtube/viewNewlogin.php
@@ -4,9 +4,20 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta charset="utf-8">
 		<title>Youtube and Facebook Analytics</title>
-		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/bootstrap/dist/css/bootstrap.css">
+		<!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+    <!-- CSS Libs -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/animate.css/animate.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/iCheck/skins/flat/_all.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/DataTables/media/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/select2/dist/css/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/vendor/css/dataTables.bootstrap.css">
+    <!-- CSS App -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/css/style.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/flat-admin-bootstrap-templates/css/themes.css">
 		<!--[if lt IE 9]>
@@ -30,8 +41,8 @@
             <!-- Show Login if the OAuth Request URL is set -->
             <form class="form col-md-12 center-block">
             <div class="form-group">
-                 <a class="btn btn-danger <?php if(isset($_SESSION['access_token'])) echo 'disabled'; else echo '';?>" aria-hidden="true" href='<?php echo $authUrl; ?>'>Login with Google</a>
-                 <a class="btn btn-info <?php if(isset($_SESSION['access_token'])) echo ''; else echo 'disabled';?>" aria-hidden="true" href='<?php echo $fb_authUrl; ?>'>Login with Facebook</a>
+                 <a class="btn btn-danger btn-lg <?php if(isset($_SESSION['access_token'])) echo 'disabled'; else echo '';?>" aria-hidden="true" href='<?php echo $authUrl; ?>'><span class="icon fa fa-youtube"></span> Login with Google</a>
+                 <a class="btn btn-info btn-lg <?php if(isset($_SESSION['access_token'])) echo ''; else echo 'disabled';?>" aria-hidden="true" href='<?php echo $fb_authUrl; ?>'><span class="icon fa fa-facebook"></span> Login with Facebook</a>
             </div>
             </form>
           </div>      
@@ -49,8 +60,23 @@
     </div>
   </div>
 </div>
-	<!-- script references -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-		<script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Javascript Libs -->
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/chartjs/Chart.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/bootstrap-switch/dist/js/bootstrap-switch.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/iCheck/icheck.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/matchHeight/jquery.matchHeight-min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/DataTables/media/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/bower_components/select2/dist/js/select2.full.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/vendor/js/dataTables.bootstrap.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/vendor/js/ace/ace.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/vendor/js/ace/mode-html.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/vendor/js/ace/theme-github.js"></script>
+    <!-- Javascript -->
+    <script type="text/javascript" src="<?php echo base_url()?>public/flat-admin-bootstrap-templates/js/app.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/js/formatting.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>public/js/waiting.js"></script>
+
 	</body>
 </html>
