0be64a64cf4e85708b26dc88e4a2d8c00472f29a
pratikone <pratik.preet@gmail.com>
2015-12-20 15:00:48 +0530

    trying to fix CORS issue

--- application/views/youtube/viewNewlogin.php
+++ application/views/youtube/viewNewlogin.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<meta charset="utf-8">
 		<title>Youtube and Facebook Analytics</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<!-- Fonts -->
